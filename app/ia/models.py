ca66f62dcced5ae3390ab1997a8179e3238c5a6a
pierrefenner <pierrefenner@hotmail.com>
2021-09-28 21:07:36 -0300

    iniciado projeto django, criação do app ia e pasta uploads

---
+++ app/ia/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+
+#biblioteca nativa Python
+from decimal import Decimal
+#bibliotecas Django
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+#bibliotecas h2o
+import h2o
+from h2o.automl import H2OAutoML
+#biblioteca pandas
+import pandas as pd
+
+#Modelo que representa a tabela no banco de dados para que o registro dos dados de processamento seja persistido
+class ModeloMachineLearningProcessado(models.Model):
+    model_id = models.TextField('Identificador do modelo', null=True)
+    #conjunto de metricas
+    auc = models.DecimalField('aux', max_digits=10, decimal_places=6, null=True)
+    logloss = models.DecimalField('logloss', max_digits=10, decimal_places=6, null=True)
+    aucpr = models.DecimalField('aucpr', max_digits=10, decimal_places=6, null=True)
+    mean_per_class_error = models.DecimalField('mean_per_class_error', max_digits=10, decimal_places=6, null=True)
+    rmse = models.DecimalField('rmse', max_digits=10, decimal_places=6, null=True)
+    mse = models.DecimalField('mse', max_digits=10, decimal_places=6, null=True)
+    #binario do modelo para a previsão : saber onde o modelo ta
+    binario_modelo = models.FileField('Binário do modelo ML', upload_to='binario_modelo_ml', null=True)
+
+
+#Modelo que representa a tabela no banco de dados para o processo
+class ProcessamentoModeloMachineLearning(models.Model):
+    data = models.DateTimeField('Data e hora do processamento', auto_now_add=True)
+    dados_csv = models.FileField('Arquivo CSV', upload_to='arquivos_csv')
+    #campo de resultado
+    classe = models.CharField('Classe', max_length=30)
+    variaveis_independentes = models.TextField('Variáveis independentes', null=True)
+    tempo_maximo = models.PositiveIntegerField('Tempo máximo em segundos', 
+                                               validators=[MinValueValidator(settings.TEMPO_MINIMO_PROCESSAMENTO), 
+                                                           MaxValueValidator(settings.TEMPO_MAXIMO_PROCESSAMENTO)],)
+    modelos_processados = models.ManyToManyField('ia.ModeloMachineLearningProcessado')
+    
+    class Meta:
+        ordering = ['-data']
+
+    def processar(self):
+        """
+        Método que processa o Auto Machine Learning e guarda os resultados do melhor modelo ranqueado
+        nos atributos do modelo da ORM Django ProcessamentoModeloMachineLearning
+        """
+        h2o.init()
+        # Importa dados do CSV que foi gravado no atributo dados_csv do modelo da ORM
+        imp = pd.read_csv(self.dados_csv, sep=";")
+
+        # Identifica dinamicamente as colunas do arquivo CSV
+        colunas = imp.columns.tolist()
+
+        # Seleciona as variáveis independentes de forma excludente, considerando a classe
+        variaveis_independentes = [coluna for coluna in colunas if coluna != self.classe]
+        self.variaveis_independentes = ','.join(variaveis_independentes)
+        self.save()
+
+        # Divide em treino e teste
+        imp = h2o.H2OFrame(imp)
+        treino, teste = imp.split_frame(ratios=[.7])
+
+        # Transforma a variável dependente em fator
+        treino[self.classe] = treino[self.classe].asfactor()
+        teste[self.classe] = teste[self.classe].asfactor()
+
+        # Auto ML
+        # Busca o modelo valor gravado no atributo tempo_maximo segundos, podemos em vez disso definir max_models
+        modelo_automl = H2OAutoML(max_runtime_secs=self.tempo_maximo, sort_metric='AUC')
+        modelo_automl.train(y=self.classe, training_frame=treino)
+
+        # Ranking dos melhores AutoML
+        ranking = modelo_automl.leaderboard
+        ranking = ranking.as_data_frame()
+
+        # Salva os resultados dos modelos rankeados associados ao processamento
+        for i in range(0, len(ranking)-1):
+            modelo_processado = ModeloMachineLearningProcessado()
+            modelo_processado.model_id = ranking['model_id'].iloc[i]
+            modelo_processado.auc = ranking['auc'].iloc[i].astype(Decimal)
+            modelo_processado.logloss = ranking['logloss'].iloc[i].astype(Decimal)
+            modelo_processado.aucpr = ranking['aucpr'].iloc[i].astype(Decimal)
+            modelo_processado.mean_per_class_error = ranking['mean_per_class_error'].iloc[i].astype(Decimal)
+            modelo_processado.rmse = ranking['rmse'].iloc[i].astype(Decimal)
+            modelo_processado.mse = ranking['mse'].iloc[i].astype(Decimal)
+            modelo = h2o.get_model(modelo_processado.model_id)
+            modelo_processado.binario_modelo.name = h2o.save_model(modelo, path="%s/modelo" % settings.MEDIA_ROOT, force=True)
+            modelo_processado.save()
+            self.modelos_processados.add(modelo_processado)
---
+++ app/ia/views.py
@@ -0,0 +1,73 @@
+import h2o
+import pandas as pd
+
+
+from rest_framework import generics
+from rest_framework.response import Response
+
+from .models import ProcessamentoModeloMachineLearning, ModeloMachineLearningProcessado
+from .serializers import ProcessamentoModeloMachineLearningCreateSerializer, PrevisaoSerializer
+
+#Herda CreateAPIView de rest_framework que está em generics
+class ProcessamentoModeloMachineLearningView(generics.CreateAPIView):
+    # importamos o serializador criado
+    serializer_class = ProcessamentoModeloMachineLearningCreateSerializer
+
+    # função para salvar, sobreescreve o método da classe CreateApiView
+    def perform_create(self, serializer):
+        # salva o modelo e retorna código 200
+        q = serializer.save()
+        #no nosso caso além de gravar os dados precisamos processar - treinar o modelo
+        # gera dados do modelos processados
+        q.processar()
+
+
+class PrevisaoView(generics.views.APIView):
+    serializer_class = PrevisaoSerializer
+
+    # sobreescreve método post de ApiView
+    def post(self, request):
+        try:
+            # Recebe os dados enviados pela requisição
+            model_id = request.POST.get('model_id')
+            csv_prever = request.FILES['csv_prever']
+
+            if model_id:
+                # Busca o modelo usando a ORM do Django pelo model_id
+                modelo_processado = ModeloMachineLearningProcessado.objects.get(model_id=model_id)
+
+                # Busca o processamento vinculado ao modelo processado usando a ORM do Django
+                processamento = modelo_processado.processamentomodelomachinelearning_set.first()
+            else:
+                # Se não informou o model_id, busca-se o melhor modelo do processamento mais recente
+                processamento = ProcessamentoModeloMachineLearning.objects.all().first() #o processamento mais recente é o first porquê a ordenação padrão da queryset é descendente do campo data
+                modelo_processado = processamento.modelos_processados.all().first() #o primeiro modelo sempre é o melhor encontrado
+
+            # Faz a leitura do arquivo para previsão fazendo uso da biblioteca Pandas
+            teste = pd.read_csv(csv_prever, sep=";")
+            colunas_enviadas = ','.join(teste.columns.tolist())
+            if processamento.variaveis_independentes != colunas_enviadas:
+                raise Exception('Erro no layout do arquivo de previsão: Para este modelo são esperadas as seguintes colunas: "{variaveis_independentes}", mas você enviou as colunas: "{colunas_enviadas}"'.format(variaveis_independentes=processamento.variaveis_independentes, colunas_enviadas=colunas_enviadas))
+
+            # Inicializa a conexão com o h2o
+            h2o.init()
+            teste = h2o.H2OFrame(teste)
+
+            # Fazer o load do binário do modelo
+            modelo_automl = h2o.load_model(modelo_processado.binario_modelo.name)
+            prever = modelo_automl.predict(teste)
+
+            data_frame = prever.as_data_frame()
+
+            """ Formatar os dados de uma forma mais simples, para percorrer depois no JavaScript """
+            previsoes = list()
+            for i in range(0, len(data_frame['predict'])-1):
+                previsoes.append({
+                    'predict': data_frame['predict'][i],
+                    'p0': data_frame['p0'][i],
+                    'p1': data_frame['p1'][i]
+                })
+
+            return Response(status=201, data={'previsoes': previsoes})
+        except Exception as e:
+            return Response(status=401, data={'Erro': str(e)})
